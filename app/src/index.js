d65e8abe4427dd569b4a7a49dc9673882e6d5b53
Giacomo Agnetti <jackss.code@gmail.com>
2017-11-23 23:36:02 +0100

    Initial commit

---
+++ app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//require('file-loader?name=[name].[ext]!../index.html');
+
+import App from './view/App'
+
+ReactDOM.render(<App/>, document.getElementById('root'))
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack')
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+const APP_DIR = path.resolve(__dirname, 'app');
+
+module.exports = {
+  entry: APP_DIR + '/src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    publicPath: "/dist/",
+    path: path.join(__dirname, 'dist')
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.template.html',
+      filename: 'index.html',
+      title: 'Spesa Counter',
+      inject: 'body'
+    })
+  ],
+
+  devServer: {
+    contentBase: './dist'
+  }
+}
