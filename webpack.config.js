91bc6a58e98653162285f9946641e0da61fbc4bd
Giacomo Agnetti <jackss.code@gmail.com>
2017-11-28 03:26:26 +0100

    Generating source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,46 @@ const webpack = require('webpack')
 const path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
 const APP_DIR = path.resolve(__dirname, 'app');
 
 module.exports = {
-  entry: APP_DIR + '/src/index.js',
+    entry: APP_DIR + '/src/index.js',
 
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  },
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
 
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.jsx$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.jsx$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
 
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './app/index.template.html',
-      filename: 'index.html',
-      title: 'Spesa Counter',
-      inject: 'body'
-    })
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/index.template.html',
+            filename: 'index.html',
+            title: 'Spesa Counter',
+            inject: 'body'
+        })
+    ],
 
-  devServer: {
-    contentBase: './dist'
-  }
+    devServer: {
+        contentBase: './dist'
+    },
+
+    devtool: 'source-map'
 }
