a7538a7009910030c49a101c1e9faffdf52476b8
Giacomo Agnetti <jackss.code@gmail.com>
2017-12-06 21:45:26 +0100

    Added header class

---
+++ app/src/view/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className='header'>
+                <div className='logo'>Spesa</div>
+            </div>
+        )
+    }
+}
+
+export default Header
