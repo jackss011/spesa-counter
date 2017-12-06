2c9b2b86e089d23a6a1c0a4d29a2dfa34e7a9c79
Giacomo Agnetti <jackss.code@gmail.com>
2017-12-06 17:11:22 +0100

    Added InfoText class

---
+++ app/src/view/generic/InfoText.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+class InfoText extends React.Component {
+    render() {
+        return <div className="info-text">{this.props.text}</div>;
+    }
+}
+
+export default InfoText
