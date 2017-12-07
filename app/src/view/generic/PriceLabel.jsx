d7d74e2ecaecc4b12a741f6e3d21162d977d938c
Giacomo Agnetti <jackss.code@gmail.com>
2017-12-07 21:01:54 +0100

    Create TotalLabel class

--- app/src/view/generic/PriceLabel.jsx
+++ app/src/view/generic/PriceLabel.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 
 class PriceLabel extends React.Component {
     render() {
-        const price = this.props.price.toFixed(2);
+        const price = this.props.price|| 0;
 
         return(
                 <div className="price-label">
-                    {price}
+                    {price.toFixed(2)}
                     <span className="currency">
                         {this.getCurrency()}
                     </span>
