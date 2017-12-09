ea714a608c5098dd19d60e67d029c05a0452a9b2
Giacomo Agnetti <jackss.code@gmail.com>
2017-12-09 01:25:41 +0100

    Added icon to app

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 require('../styles/main.sass')
+require('../res/favicon.ico')
 
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
